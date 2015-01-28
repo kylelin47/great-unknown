db53d4607c175bca9bf3a4d7e5b58937191da75d
Kyle Lin <kylelin47@gmail.com>
2015-01-28 14:28:57 -0500

    Added tweet button

--- public/modules/podcasts/services/podcasts.client.service.js
+++ public/modules/podcasts/services/podcasts.client.service.js
@@ -11,6 +11,21 @@ angular.module('podcasts').factory('Podcasts', ['$resource',
 		});
 	}
 ]);
+// From endorama @ https://gist.github.com/endorama/7369006
+angular.module('podcasts').directive('script', function() {
+    return {
+      restrict: 'E',
+      scope: false,
+      link: function(scope, elem, attr) {
+        if (attr.type==='text/javascript-lazy') {
+          var code = elem.text();
+		  /*jslint evil: true */
+          var f = new Function(code);
+          f();
+        }
+	  }
+	};
+});
 angular.module('podcasts').directive('fileModel', ['$parse', function ($parse) {
     return {
         restrict: 'A',
