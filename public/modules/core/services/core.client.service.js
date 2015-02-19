baa2614ec8e2c92971ee01faad91e8dae7d79f6c
qianwang1013 <qianwang1013@hotmail.com>
2015-02-18 21:45:50 -0500

    This services is only responsible for reading the feed url

---
+++ public/modules/core/services/core.client.service.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('core').factory('FeedService',['$http',function($http){
+    return {
+        parseFeed : function(url){
+            return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+        }
+    };
+}]);
