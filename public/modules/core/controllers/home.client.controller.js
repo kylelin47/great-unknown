2f94520e2d4f6f79724db0081e507f8853f59e07
qianwang1013 <qianwang1013@hotmail.com>
2015-02-19 01:15:13 -0500

    modal functions is now working and loaded with test results

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,40 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication','$modal','$log',
+	function($scope, Authentication, $modal, $log) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+
+        //***************************Modal window for Rss feed********************************//
+        $scope.items = ['item1', 'item2', 'item3'];
+
+        $scope.modalOpen = function(size) {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'test.html',
+                controller: function($scope, $modalInstance,items){
+                    $scope.items = items;
+
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
+                size: size,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+        //***************************Modal window for Rss feed********************************//
 	}
 ]);
