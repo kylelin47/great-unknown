98356c1ebbfb0182232d8826401b75089ffe62fe
qianwang1013 <qianwang1013@hotmail.com>
2015-03-01 22:18:40 -0500

    Feed reader now reads in Rss feed and modal output

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,17 +1,28 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication','$modal','$log',
-	function($scope, Authentication, $modal, $log) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication','$modal', '$log', 'FeedService',
+	function($scope, Authentication, $modal,$log, FeedService) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+        $scope.data = {
+            feedSrc:''
+        }
 
 
         //***************************Modal window for Rss feed********************************//
 
         $scope.modalOpen = function(size) {
 
+            FeedService.getFeed($scope.data);
+
+            //set the feedSrc value into factory
+
+
             var modalInstance = $modal.open({
+
+
                 templateUrl: 'modules/core/views/feed.client.view.html',
                 controller: function($scope, $modalInstance,items){
                     $scope.items = items;
--- public/modules/core/services/core.client.service.js
+++ public/modules/core/services/core.client.service.js
@@ -1,7 +1,18 @@
 'use strict';
 
 angular.module('core').factory('FeedService',['$http',function($http){
+
+    var myObj = {
+        feedSrc: ''
+    };
+
     return {
+        getFeed: function(data){
+            myObj = data;
+        },
+        setFeed: function(){
+            return myObj;
+        },
         parseFeed : function(url){
             return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
         }
