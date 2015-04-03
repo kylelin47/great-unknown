31db27a396de3f2e56be354f2501155d5004e726
qianwang1013 <qianwang1013@hotmail.com>
2015-04-02 23:43:01 -0400

    Subscribe button will send out an email to the user.
    This functionality is still not finished.
     Testing email address is set to mine gmail acc. I will change that to the current log in visitor later.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -1,5 +1,27 @@
 'use strict';
 
+var nodemailer = require('nodemailer');
+
+var user;
+var transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+        user: 'qianwang1013@hotmail.com',
+        pass: '13564588122'
+    }
+});
+var email_text = 'You have successfully subscribe to my blog \n'+
+                    'I hope you will have a great time \n';
+    transporter.sendMail({
+        from: 'qianwang1013@hotmail.com',
+        to: 'qianwang1013@gmail.com',
+        subject: 'Welcome to my Podcast',
+        text: email_text
+
+    });
+
+
+
 /**
  * Module dependencies.
  */
