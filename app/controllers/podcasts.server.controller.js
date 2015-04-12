7bafb8f0a83761519e8e3e73fc68441ba1a51909
qianwang1013 <qianwang1013@hotmail.com>
2015-04-12 18:42:29 -0400

    Removing my email info. That is not nessary.

--- app/controllers/podcasts.server.controller.js
+++ app/controllers/podcasts.server.controller.js
@@ -66,7 +66,6 @@ var sendNotification = function(){
             //console.log(subscriber_list[i].email);
             var message = {
                 message: {
-                    from_email: 'qianwang1013@gmail.com',
                     from_name: 'Podcast Manager',
                     to: [{email: subscriber_list[i].email}],
                     subject: 'Check out what is new!',
